7c8d5c01a51557d5b1afe26ffa2c863ea62d5070
facundotopa <facundotopa738@gmail.com>
2022-11-22 11:02:37 -0300

    Intro-uno-js : este es mi segundo ejercicio en JS

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-let ciudad="General Pinto"
+let nombre="Facundo Topa"
 let numero=3*Math.random()
-const nombre = "Facundo"
-const apellido = "Topa"
-const nombrecompleto=`${nombre} ${apellido}`
+console.log("numero random",numero,"Nombre",nombre)
 
-console.log("numero random",numero,"Nombre de ciudad",ciudad)
-console.log("Nombre completo",nombrecompleto)
+
+const celular= {
+    key: '0203'
+    ,
+    marca:'samsung'
+        ,
+    color:'uzul',
+        precio:'9000',
+    anio_creacion:'2020'
+
+}
+console.log(celular)
+console.log(celular.key)
+celular.key=true
+celular.anio_creacion=2017
+console.log(delete celular.marca);
+console.log(Object.keys(celular));
+
+const arreglo=[1,2,3,4]
+
+let arreglo2=arreglo
+arreglo2.push(5)
+console.log(arreglo2)
+const arreglo3 = arreglo2.map(x => x * 5);
+
+const person = {
+    name: 'Pepe',
+    age: 26,
+    hobbies: ['chess', 'running', 'basket']
+}
+const nombre=person.name
+const edad = person.age
+const hobbies= person.hobbies
+
+console.log(person)
